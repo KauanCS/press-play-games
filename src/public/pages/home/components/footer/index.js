fad7c4e84a6b4b6a396962774ae101407f07c127
Kauan <kauan13cs@gmail.com>
2021-07-06 21:19:07 -0300

    Removing unncessary code

--- src/public/pages/home/components/footer/index.js
+++ src/public/pages/home/components/footer/index.js
@@ -39,79 +39,76 @@ import {
   FOOTER_DEVELOP_BY,
 } from '../../../../constants/public-home';
 
-const Footer = () => {
-  const title = 'title';
-  return (
-    <Container>
-      <ContainerContent>
-        <ContainerLeft>
-          <Title fontSize="16px">{FOOTER_CONTACT}</Title>
-          <ContainerText href={FOOTER_WHATSAPP_LINK}>
-            <IconWhatsapp />
-            <Text>{FOOTER_WHATSAPP}</Text>
+const Footer = () => (
+  <Container>
+    <ContainerContent>
+      <ContainerLeft>
+        <Title fontSize="16px">{FOOTER_CONTACT}</Title>
+        <ContainerText href={FOOTER_WHATSAPP_LINK}>
+          <IconWhatsapp />
+          <Text>{FOOTER_WHATSAPP}</Text>
+        </ContainerText>
+
+        <ContainerText noCursor>
+          <IconDiscord />
+          <Text>{FOOTER_DISCORD}</Text>
+        </ContainerText>
+
+        <ContainerText noCursor>
+          <IconEnvelope />
+          <Text>{FOOTER_EMAIL}</Text>
+        </ContainerText>
+
+        <ContainerText href={FOOTER_FACEBOOK_LINK}>
+          <IconFacebook />
+          <Text>{FOOTER_FACEBOOK}</Text>
+        </ContainerText>
+
+        <ContainerText href={FOOTER_INSTAGRAM_LINK}>
+          <IconInstagram />
+          <Text>{FOOTER_INSTAGRAM}</Text>
+        </ContainerText>
+      </ContainerLeft>
+
+      <ContainerMid>
+        <Title>{FOOTER_LIKE_PAGE}</Title>
+        <iframe
+          title="facebook-plugin"
+          src={FOOTER_FACEBOOK_SRC}
+          width="425"
+          height="130"
+          style={{ border: 'none', overflow: 'hidden' }}
+          scrolling="no"
+          frameBorder="0"
+          allowFullScreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        />
+      </ContainerMid>
+
+      <ContainerRight>
+        <Title fontSize="16px">{FOOTER_LINKS}</Title>
+
+        <ContainerItemsRight>
+          <ContainerText>
+            <Text>{FOOTER_HOW_IT_WORKS}</Text>
           </ContainerText>
 
-          <ContainerText noCursor>
-            <IconDiscord />
-            <Text>{FOOTER_DISCORD}</Text>
+          <ContainerText>
+            <Text>{FOOTER_INSTALLATION}</Text>
           </ContainerText>
 
-          <ContainerText noCursor>
-            <IconEnvelope />
-            <Text>{FOOTER_EMAIL}</Text>
+          <ContainerText>
+            <Text>{FOOTER_PLANS_PRICE}</Text>
           </ContainerText>
+        </ContainerItemsRight>
 
-          <ContainerText href={FOOTER_FACEBOOK_LINK}>
-            <IconFacebook />
-            <Text>{FOOTER_FACEBOOK}</Text>
-          </ContainerText>
-
-          <ContainerText href={FOOTER_INSTAGRAM_LINK}>
-            <IconInstagram />
-            <Text>{FOOTER_INSTAGRAM}</Text>
-          </ContainerText>
-        </ContainerLeft>
-
-        <ContainerMid>
-          <Title>{FOOTER_LIKE_PAGE}</Title>
-          <iframe
-            title="facebook-plugin"
-            src={FOOTER_FACEBOOK_SRC}
-            width="425"
-            height="130"
-            style={{ border: 'none', overflow: 'hidden' }}
-            scrolling="no"
-            frameBorder="0"
-            allowFullScreen="true"
-            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          />
-        </ContainerMid>
-
-        <ContainerRight>
-          <Title fontSize="16px">{FOOTER_LINKS}</Title>
-
-          <ContainerItemsRight>
-            <ContainerText>
-              <Text>{FOOTER_HOW_IT_WORKS}</Text>
-            </ContainerText>
-
-            <ContainerText>
-              <Text>{FOOTER_INSTALLATION}</Text>
-            </ContainerText>
-
-            <ContainerText>
-              <Text>{FOOTER_PLANS_PRICE}</Text>
-            </ContainerText>
-          </ContainerItemsRight>
-
-        </ContainerRight>
-      </ContainerContent>
-      <BottomFooter>
-        <BottomText>{FOOTER_COPYRIGHT}</BottomText>
-        <BottomText>{FOOTER_DEVELOP_BY}</BottomText>
-      </BottomFooter>
-    </Container>
-  );
-};
+      </ContainerRight>
+    </ContainerContent>
+    <BottomFooter>
+      <BottomText>{FOOTER_COPYRIGHT}</BottomText>
+      <BottomText>{FOOTER_DEVELOP_BY}</BottomText>
+    </BottomFooter>
+  </Container>
+);
 
 export default Footer;
